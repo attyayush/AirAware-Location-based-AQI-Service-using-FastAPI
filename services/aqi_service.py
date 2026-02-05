62e92b1165ff1b4fce47c636e252e4d29394f781
attyayush <158755664+attyayush@users.noreply.github.com>
2026-02-05 10:36:32 +0530

    Update aqi_service.py

--- services/aqi_service.py
+++ services/aqi_service.py
@@ -1,5 +1,9 @@
 import httpx
 from loguru import logger
+from cachetools import TTLCache 
+
+
+aqi_cache = TTLCache(maxsize=100, ttl=900)
 
 def get_aqi_category(aqi_value: int) -> str:
     """Standard US EPA AQI Categories"""
@@ -11,6 +15,12 @@ def get_aqi_category(aqi_value: int) -> str:
     return "Hazardous"
 
 async def get_air_quality(lat: float, lon: float):
+   
+    cache_key = f"{lat}_{lon}"
+    if cache_key in aqi_cache:
+        logger.info(f"Returning CACHED AQI for {lat}, {lon}")
+        return aqi_cache[cache_key]
+
     url = "https://air-quality-api.open-meteo.com/v1/air-quality"
     params = {
         "latitude": lat,
@@ -18,17 +28,27 @@ async def get_air_quality(lat: float, lon: float):
         "current": "us_aqi"
     }
     
-    async with httpx.AsyncClient() as client:
+    
+    async with httpx.AsyncClient(timeout=10.0) as client:
         try:
-            response = await client.get(url, params=params, timeout=5.0)
+            logger.info(f"Fetching fresh AQI from Open-Meteo for {lat}, {lon}")
+            response = await client.get(url, params=params)
             response.raise_for_status()
             data = response.json()
             
             aqi_val = data["current"]["us_aqi"]
-            return {
+            result = {
                 "value": int(aqi_val),
                 "category": get_aqi_category(aqi_val)
             }
+
+            
+            aqi_cache[cache_key] = result
+            return result
+
+        except httpx.TimeoutException:
+            logger.error("AQI service timed out")
+            return None
         except Exception as e:
             logger.error(f"AQI service error: {e}")
             return None
