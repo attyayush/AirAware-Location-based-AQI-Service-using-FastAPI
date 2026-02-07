3788cd518c744e967a2bca31726b5671149c48cd
attyayush <158755664+attyayush@users.noreply.github.com>
2026-02-07 15:34:23 +0530

    Update aqi.py

--- routers/aqi.py
+++ routers/aqi.py
@@ -5,6 +5,7 @@ from services.ip_service import get_client_ip
 from services.geo_service import get_location_data
 from services.aqi_service import get_air_quality
 from datetime import datetime
+from services.geo_service import get_location_data, get_coords_by_city
 
 router = APIRouter()
 
@@ -38,4 +39,26 @@ async def get_stats():
 
 @router.get("/aqi/history")
 async def get_history():
-    return await get_recent_history(5)
+    return await get_recent_history(10)
+@router.get("/aqi/by-city", response_model=AirQualityResponse)
+async def get_aqi_by_city(name: str):
+    
+    geo_data = await get_coords_by_city(name)
+    if not geo_data:
+        raise HTTPException(status_code=404, detail=f"City '{name}' not found")
+    
+   
+    aqi_data = await get_air_quality(geo_data["lat"], geo_data["lon"])
+    
+  
+    result = {
+        "ip": "City-Search",
+        "location": geo_data,
+        "aqi": aqi_data,
+        "source": "open-meteo",
+        "timestamp": datetime.utcnow()
+    }
+    
+    await log_aqi_request(result)
+    
+    return result
